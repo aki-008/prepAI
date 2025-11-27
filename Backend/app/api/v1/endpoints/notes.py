bb01e85c276a17c9d35bf57a037d059bfb323dd2
SHASWATSINGH3101 <shaswatsingh910@gmail.com>
2025-11-27 14:37:44 +0000

    feat: upload notes endpoint

--- Backend/app/api/v1/endpoints/notes.py
+++ Backend/app/api/v1/endpoints/notes.py
@@ -1,18 +1,29 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status, File, UploadFile
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.models import User
 from app.api.deps import get_db, get_current_user
-from app.schema import ChatMessage, AI_chat_input
-from .prompts import SYSTEM_PROMPT
-from app.llm import call_llm, stream_chat
+from app.schema import ChatMessage, AI_chat_input, pdf_input
+from app.llm import stream_chat
 import uuid
 from fastapi.responses import StreamingResponse
-
-
+from chromadb.api.models.Collection import Collection 
+from app.api.deps import get_chroma_collection
+from app.api.deps import get_db, get_current_user, get_chroma_client
+from pathlib import Path
+from llama_index.readers.file import PyMuPDFReader
+from llama_index.core.node_parser import SentenceSplitter
+from typing import Annotated
+import shutil
+import os
+from .quiz import ingest_logic
 
 router = APIRouter(prefix="/notes")
 
-@router.post("/stram_chat", response_class=StreamingResponse)
+UPLOAD_DIRECTORY = "uploaded_pdfs"
+os.makedirs(UPLOAD_DIRECTORY, exist_ok=True)
+
+
+@router.post("/stream_chat", response_class=StreamingResponse)
 async def ai_chat(
     Input_model: AI_chat_input, 
     # db: AsyncSession = Depends(get_db),
@@ -24,3 +35,49 @@ async def ai_chat(
         stream_chat(messages_dict, Input_model.context),
         media_type="text/plain"
     )
+
+@router.post("/upload_notes")
+async def upload_notes(
+    file: Annotated[UploadFile, File(description="A PDF file to upload")],
+    collection: Collection = Depends(get_chroma_collection), 
+    db: AsyncSession = Depends(get_db),
+    current_user: User = Depends(get_current_user)
+):
+    file_path = Path(UPLOAD_DIRECTORY) / file.filename
+    
+    try:
+
+        chunks = await pdf_process(str(file_path))
+        if not chunks:
+            raise ValueError("No chunks availible")
+
+        await ingest_logic(chunks, collection)
+
+        return {"status": "success"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error processing PDF: {str(e)}")
+        
+    finally:
+        if file_path.exists():
+            os.remove(file_path)
+
+# #--------Helper Functions--------#
+
+async def pdf_process(pdf_path: str):
+    loader = PyMuPDFReader()
+    
+    # 5. Load using the file path string
+    documents = loader.load_data(file_path=pdf_path)
+    
+    text_splitter = SentenceSplitter(
+        chunk_size=1000,
+        chunk_overlap=20
+    )
+    
+    text_chunks = []
+    
+    for doc_idx, doc in enumerate(documents):
+        cur_text_chunks = text_splitter.split_text(doc.text)
+        text_chunks.extend(cur_text_chunks)
+
+    return text_chunks
--- Backend/app/models/tables.py
+++ Backend/app/models/tables.py
@@ -1,8 +1,8 @@
-from sqlalchemy import String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, LargeBinary, JSON, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from datetime import datetime
 from app.database import Base
-
+from typing import List
 
 class User(Base):
     __tablename__ = "users"
@@ -12,3 +12,14 @@ class User(Base):
     email: Mapped[str] = mapped_column(String(100), unique=True, index=True)
     hashed_password: Mapped[str] = mapped_column(String(255))
 
+    pdf_data: Mapped[list["PDFData"]] = relationship(back_populates="user")
+
+class PDFData(Base):
+    __tablename__ = "pdf_data"
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    pdf_blob: Mapped[bytes] = mapped_column(LargeBinary)
+    messages_list: Mapped[List] = mapped_column(JSON)
+    pdf_embedding: Mapped[list[float]] = mapped_column(JSON)
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
+    user: Mapped["User"] = relationship(back_populates="pdf_data")
